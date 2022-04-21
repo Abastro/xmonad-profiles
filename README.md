a84630aefb61e578abd41cb4a3536b752d8093e5
Abastro <abab9579@gmail.com>
2022-04-21 17:37:30 +0900

    Change of installations

--- README.md
+++ README.md
@@ -15,17 +15,25 @@ Requires:
 Procedure:
 1. `git clone` this repository at suitable location to manage the profiles inside.
 2. `cd xmonad-profiles` to get into the repo directory.
-3. Run `install/Setup.hs . [profile-name]` to install required components.
+3. Run `./install.sh` to install `xmonad-manage`
   - Likely requires granting executable permission.
-  - Can install multiple profiles.
+  - Adds `xmonad-manage` executable `$HOME/.cabal/bin`. This might change in near future.
+4. Run `xmonad-manage install` to install required components.
+5. Run `xmonad-manage install [profile-name]` to install the specified profile.
   - Currently supported profiles: `pulpmonad`, `xmonad-test`
 
 This procedure automatically installs profiles in `/usr/share/xsessions/`,
 which you can select from login screen.
 
+## XMonad-Manage
+`xmonad-manage` is a tool to manage xmonad profiles.
+- `xmonad-manage install`: Installs common components.
+- `xmonad-manage install [profile-name]`: Installs profile `[profile-name]`.
+- `xmonad-manage build [profile-name]`: Builds config for profile `[profile-name]`
+- `xmonad-manage run [profile-name]`: Runs XMonad instance of profile `[profile-name]`
+
 ### Notes
 
 NB: Removal method is not yet supported; this is due to `cabal` lacking uninstall/gc methods.
 
-If you don't mind nuking the haskell stores,
-you can remove this directory and `$HOME/.ghcup`, `$HOME/.cabal` to do the cleanup.
+If you don't mind nuking the haskell stores, you can remove the `xmonad-profiles` directory and `$HOME/.ghcup`, `$HOME/.cabal` to do the cleanup.
--- xmonad-manage/app/Main.hs
+++ xmonad-manage/app/Main.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NoFieldSelectors #-}
 
 module Main (main) where
 
+import Control.Exception
 import Control.Monad.Except
 import Data.Foldable
+import Data.Text qualified as T
 import System.Directory
 import System.Environment
 import System.FilePath
@@ -12,29 +15,46 @@ import System.IO
 import System.Process
 import Text.Printf
 
+-- TODO Refactor
+
 -- | Denotes executable on PATH.
 newtype Executable = Executable FilePath
 
 type XMonad = Executable
 
+-- | Profile ID, restricted names
 type ProfileID = String
+
+-- | Build Info
+data BuildInfo = BuildInfo
+  { xmonadExe :: !String,
+    otherExes :: [String]
+  }
+  deriving (Read, Show)
+
+-- | Profile Config
+data ProfileCfg = ProfileCfg
+  { profileID :: !ProfileID,
+    profileName :: !T.Text,
+    installScript :: !(Maybe FilePath),
+    builds :: !BuildInfo
+  }
+  deriving (Read, Show)
 
 -- | Profile. Requires the config path to exist.
 data Profile = Profile
-  { profID :: !ProfileID,
-    dataDir :: !FilePath,
-    cfgDir :: !FilePath,
-    cacheDir :: !FilePath,
-    logDir :: !FilePath,
+  { profCfg :: !ProfileCfg,
+    cfgDir, dataDir, handleDir, cacheDir, logDir :: !FilePath,
     starter :: !FilePath
   }
 
 data ManageError
   = ExeNotFound String
   | ProfileNotFound ProfileID
-  | IllegalArgs [String]
+  | ProfileWrongFormat ProfileID String
+  deriving (Show)
 
-type ManageStack = ExceptT ManageError IO
+instance Exception ManageError
 
 -- | Calls the executable.
 callExe :: Executable -> [String] -> IO ()
@@ -51,20 +71,26 @@ setToExecutable path = do
   Executable path <$ setPermissions path (setOwnerExecutable True perm)
 
 -- | Gets an executable.
-getExecutable :: String -> ManageStack Executable
+getExecutable :: String -> IO Executable
 getExecutable exe =
-  liftIO (findExecutable exe) >>= \case
-    Nothing -> throwError (ExeNotFound exe)
+  (findExecutable exe) >>= \case
+    Nothing -> throwIO (ExeNotFound exe)
     Just path -> pure (Executable path)
 
 -- | Gets a profile, or gives Nothing if not found.
-getProfile :: FilePath -> ProfileID -> ManageStack Profile
+getProfile :: FilePath -> ProfileID -> IO Profile
 getProfile parent profID = do
-  liftIO (doesDirectoryExist cfgDir) >>= (`unless` throwError (ProfileNotFound profID))
-  pure (Profile {profID, dataDir, cfgDir, cacheDir, logDir, starter})
+  doesDirectoryExist cfgDir >>= (`unless` throwIO (ProfileNotFound profID))
+  cfgStr <- readFile (cfgDir </> "profile.cfg")
+  profCfg <- case reads @ProfileCfg cfgStr of
+    [(cfg, "")] -> pure cfg -- TODO Use proper parser combinators
+    remaining -> throwIO (ProfileWrongFormat profID $ errMsg $ show remaining)
+  pure (Profile {profCfg, cfgDir, dataDir, handleDir, cacheDir, logDir, starter})
   where
+    errMsg = printf "left while parsing: %s"
     dataDir = parent </> "data" </> profID
     cfgDir = parent </> profID
+    handleDir = parent </> "handle" </> profID
     cacheDir = parent </> "cache" </> profID
     logDir = parent </> "logs" </> profID
     starter = parent </> "start.sh"
@@ -72,15 +98,11 @@ getProfile parent profID = do
 -- | Initial installation.
 installInit :: Executable -> IO ()
 installInit sudo = do
-  printf "[Main] Install Begin\n"
-
   findExecutable "xmonad" >>= \case
-    Just _ -> putStrLn "[Main] xmonad found in PATH"
+    Just _ -> putStrLn "[install] xmonad found in PATH"
     Nothing -> callCommand "cabal install xmonad"
 
   callExe sudo (["apt", "install"] <> dependencies)
-
-  printf "[Main] Install End\n"
   where
     dependencies =
       ["xcompmgr", "suckless-tools", "gnome-screensaver", "xss-lock", "gnome-keyring"]
@@ -88,35 +110,71 @@ installInit sudo = do
 -- | Installs a profile - first argument is sudo.
 installProfile :: Executable -> XMonad -> Profile -> IO ()
 installProfile sudo xmonad profile@Profile {..} = do
-  printf "[%s] Install Begin\n" profID
-
-  traverse_ (createDirectoryIfMissing True) [dataDir, cacheDir, logDir]
-  installs <- setToExecutable (cfgDir </> "install")
-  _ <- setToExecutable (cfgDir </> "build")
+  traverse_ (createDirectoryIfMissing True) [dataDir, handleDir, cacheDir, logDir]
+  installs <- traverse (setToExecutable . (cfgDir </>)) installScript
+  -- _ <- setToExecutable (cfgDir </> "build")
   _ <- setToExecutable starter -- For now, let's set starter here
+
+  curDir <- getCurrentDirectory
+  writeFile (cfgDir </> "build") (builder curDir)
+  _ <- setToExecutable (cfgDir </> "build")
+
   writeFile runnerPath (runner starter)
   callExe sudo ["ln", "-sf", runnerPath, runnerLinked]
 
-  callExe installs []
+  traverse_ (`callExe` []) installs
   runXMonad xmonad profile True ["--recompile"]
-
-  printf "[%s] Install End\n" profID
   where
-    runnerLinked = "/usr" </> "share" </> "xsessions" </> profID <.> "desktop"
+    ProfileCfg {profileID, profileName, installScript} = profCfg
+    runnerLinked = "/usr" </> "share" </> "xsessions" </> profileID <.> "desktop"
     runnerPath = dataDir </> "run" <.> "desktop"
     runner starter =
       unlines
         [ printf "[Desktop Entry]",
           printf "Encoding=UTF-8",
-          printf "Name=%s" profID,
-          printf "Comment=Xmonad profile <%s>" profID,
-          printf "Exec=%s %s" (show starter) profID,
+          printf "Name=%s" profileName,
+          printf "Comment=Xmonad profile %s" profileID,
+          printf "Exec=%s %s" (show starter) profileID,
           printf "Type=XSession"
         ]
 
+    builder curDir =
+      unlines
+        [ printf "#!/bin/sh",
+          printf "cd %s || exit" (show curDir),
+          printf "exec xmonad-manage build %s %s" profileID (show "$1")
+        ]
+
+-- | Builds certain profile, called by a build script.
+buildProfile :: FilePath -> Profile -> IO ()
+buildProfile xmonadDest Profile {profCfg, cfgDir, cacheDir} = withCurrentDirectory cfgDir $ do
+  printf "[build] Building config from directory %s\n" cfgDir
+  printf "[build] XMonad config destination %s\n" xmonadDest
+
+  cabal <- getExecutable "cabal"
+  -- Run build to check if it is updated
+  need <- withCreateProcess (exeToProc cabal $ "build" : targets) {std_out = CreatePipe} $
+    \_ (Just hout) _ _ -> do -- Let's assume stdout close = build end
+      hGetContents hout >>= \l -> ("Up to date" `notElem` lines l) <$ putStr l
+
+  -- If files are rebuilt, install it
+  when need $ do
+    ln <- getExecutable "ln"
+    callExe cabal ("install" : targets <> installOpts)
+    callExe ln ["-sf", cacheDir </> xmonadExe, xmonadDest]
+    callExe cabal ("build" : targets) -- Hack to reduce checking time
+  where
+    ProfileCfg {builds = BuildInfo {xmonadExe, otherExes}} = profCfg
+    targets = map ("exe:" <>) (xmonadExe : otherExes)
+    installOpts =
+      [ printf "--installdir=%s" cacheDir,
+        printf "--install-method=copy",
+        printf "--overwrite-policy=always"
+      ]
+
 -- | Runs xmonad for profile with given options.
 runXMonad :: XMonad -> Profile -> Bool -> [String] -> IO ()
-runXMonad xmonad Profile {..} untilEnd opts = do
+runXMonad xmonad Profile {dataDir, cfgDir, cacheDir, logDir} untilEnd opts = do
   setEnv "XMONAD_DATA_DIR" dataDir
   setEnv "XMONAD_CONFIG_DIR" cfgDir
   setEnv "XMONAD_CACHE_DIR" cacheDir
@@ -124,36 +182,68 @@ runXMonad xmonad Profile {..} untilEnd opts = do
     then callExe xmonad opts
     else do
       [logs, errors] <- traverse openLog ["xmonad.log", "xmonad.err"]
-      (_, _, _, handle) <-
-        createProcess (exeToProc xmonad opts) {std_out = UseHandle logs, std_err = UseHandle errors}
-      () <$ waitForProcess handle -- TODO Can I start xmonad and close this program?
+      withCreateProcess (exeToProc xmonad opts) {std_out = UseHandle logs, std_err = UseHandle errors} $
+        \_ _ _ ph -> () <$ waitForProcess ph
   where
     openLog name = openFile (logDir </> name) WriteMode
+
+-- TODO Remove current directory dependency
 
 -- | The manager program. Current directory needs to be the profile main directory.
 main :: IO ()
 main = do
+  home <- getHomeDirectory
   parent <- getCurrentDirectory
   let setupExe = parent </> "xmonad.setup"
-  res <- runExceptT $ do
+  (`catch` handleError) $ do
     sudo <- getExecutable "sudo"
-    liftIO getArgs >>= \case
-      ["install"] -> liftIO $ do
+    getArgs >>= \case
+      -- Main installation
+      ["install"] -> do
+        printf "[install] Begin\n"
         setToExecutable setupExe
         installInit sudo
-      ["install", profID] -> do
-        inst <- installProfile sudo <$> getExecutable "xmonad" <*> getProfile parent profID
-        liftIO inst
-      ["run", profID] -> do
-        liftIO (callProcess setupExe [] >> putStrLn "Now running xmonad...")
-        runXM <- runXMonad <$> getExecutable "xmonad" <*> getProfile parent profID
-        liftIO (runXM False [])
-      args -> throwError (IllegalArgs args)
+        printf "[install] End\n"
 
-  case res of
-    Right () -> pure ()
-    Left (ExeNotFound exe) -> hPrintf stderr "Error: Executable %s not found in PATH\n" exe
-    Left (ProfileNotFound profID) -> hPrintf stderr "Error: Profile %s not found\n" profID
-    Left (IllegalArgs args) -> do
-      hPrintf stderr "Unexpected command %s\n" (show args)
-      hPrintf stderr "Supported: install, install <profile>, run <profile>\n"
+      -- Profile-specific installation
+      ["install", profID] -> do
+        printf "[install %s] Begin\n" profID
+        join $ installProfile sudo <$> getExecutable "xmonad" <*> getProfile parent profID
+        printf "[install %s] End\n" profID
+
+      -- Automatic profile build
+      ["build", profID, destFile] -> do
+        printf "[build %s %s] Begin\n" profID destFile
+        join $ buildProfile destFile <$> getProfile parent profID
+        printf "[build %s %s] End\n" profID destFile
+      -- Manual profile build
+      ["build", profID] -> do -- TODO Use binfilename instead (arch-os)
+        printf "[build %s] Begin\n" profID
+        profile@Profile {cacheDir} <- getProfile parent profID
+        buildProfile (cacheDir </> "xmonad-x86_64-linux") profile
+        printf "[build %s] End\n" profID
+
+      -- Automatic profile run
+      ["run", profID] -> do
+        printf "[run %s] Setup\n" profID
+        withCurrentDirectory home $ callProcess setupExe []
+        printf "[run %s] Booting xmonad\n" profID
+        runXM <- runXMonad <$> getExecutable "xmonad" <*> getProfile parent profID
+        withCurrentDirectory home $ runXM False []
+        printf "[run %s] Exit" profID
+
+      -- Illegal Arguments
+      args -> do
+        hPrintf stderr "Unexpected command %s\n" (show args)
+        hPrintf stderr "Supported commands:\n"
+        hPrintf stderr "install, install <profile>, build <profile> [dir], run <profile>\n"
+  where
+    -- MAYBE Do these need to be here?
+    handleError = \case
+      ExeNotFound exe -> hPrintf stderr "Error: Executable %s not found in PATH\n" exe
+      ProfileNotFound profID -> hPrintf stderr "Error: Profile %s not found\n" profID
+      ProfileWrongFormat profID cause -> do
+        hPrintf stderr "Error: Profile %s cannot be read\n" profID
+        hPrintf stderr "Cause: %s\n" cause
+        
+        
