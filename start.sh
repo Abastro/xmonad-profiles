d84736a8ff894a6660f0ec086a8a3327ed2caf21
Abastro <abab9579@gmail.com>
2022-04-22 22:44:39 +0900

    GTK2_RC_FILES move

--- start.sh
+++ start.sh
@@ -5,10 +5,9 @@ cd "$( dirname "$0" )" || exit
 # PATH setup
 export PATH="$HOME/.cabal/bin":"$HOME/.ghcup/bin":"$PATH"
 
-# TODO Move this out to personal config thing
-# For GTK2
-export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
+# TODO Ubuntu notification setup
 
+# TODO Move this out to personal config thing
 # Config ibus
 export GTK_IM_MODULE=ibus
 export XMODIFIERS=@im=ibus
@@ -16,9 +15,8 @@ export QT4_IM_MODULE=ibus
 export QT_IM_MODULE=ibus
 
 # GDK scales for HDPI
-GDK_SCALE=2
-GDK_DPI_SCALE=0.5
-QT_AUTO_SCREEN_SCALE_FACTOR=1
-export GDK_SCALE GDK_DPI_SCALE QT_AUTO_SCREEN_SCALE_FACTOR
+export GDK_SCALE=2
+export GDK_DPI_SCALE=0.5
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
 
 exec xmonad-manage run "$1" > "./logs/common.log" 2> "./logs/common.err"
