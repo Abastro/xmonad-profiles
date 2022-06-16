fb6effd80c335f7348c0108a7d97ec9444cad4ee
Abastro <abab9579@gmail.com>
2022-06-16 10:53:12 +0900

    Band-aid for xset now

--- start-lightdm/startup-run.sh
+++ start-lightdm/startup-run.sh
@@ -20,6 +20,9 @@ ibus-daemon -drx &
 # locker dedicated for lightdm
 light-locker &
 
+# TODO: Make this somehow a separate settings
+xset s 3000 dpms 3600 3600 3600
+
 # Keyring-daemon for e.g. ssh keys
 gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh
 
