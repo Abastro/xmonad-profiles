2fd5b8caf99d9f93b4e079c56c567a21a50534dd
Abastro <abab9579@gmail.com>
2023-04-10 22:57:12 +0900

    Arch-SDDM

---
+++ start-arch-sddm/startup-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Installs picom, polkit-kde-agent, gnome-keyring"
+sudo pacman -Syu picom polkit-kde-agent gnome-keyring
+
+# Install necessary components
+
---
+++ start-arch-sddm/startup-run.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+parent=$( dirname "$0" )
+
+echo "Startup for SDDM..."
+
+# Monitor settings use xrandr
+xrandr
+
+# Copy X resources
+cp -T "$parent/.Xresources" "$HOME/.Xresources"
+# Load X resources
+xrdb -merge ~/.Xresources
+
+# picom does transparency
+picom -bcCGf -D 4 -I 0.02 -O 0.02 -e 0.8
+
+# ibus-daemon provides ibus input handling
+ibus-daemon -drx &
+
+# SDDM locker?
+
+# TODO: Make this somehow a separate settings
+# xset s 3000 dpms 3600 3600 3600
+
+# KDE Authentication agent
+/usr/lib/polkit-kde-authentication-agent-1 &
+
+# Keyring-daemon for e.g. ssh keys. Need gnome for this.
+gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh
+
+# Workaround for dbus being displayed first
+dbus-update-activation-environment GDK_SCALE GDK_DPI_SCALE QT_AUTO_SCREEN_SCALE_FACTOR
+
+# Set root cursor
+xsetroot -cursor_name left_ptr
+
