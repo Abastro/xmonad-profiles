1c68f53e6ff4878e8d66c28cb963c8a1a1759cd6
Abastro <abab9579@gmail.com>
2023-05-08 22:59:00 +0900

    Considering better way to specify dependencies

--- start-basic/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Installs xcompmgr, suckless-tools, xscreensaver, xss-lock, gnome-keyring"
-
-# Install necessary components
-sudo apt install xcompmgr suckless-tools xscreensaver xss-lock gnome-keyring
