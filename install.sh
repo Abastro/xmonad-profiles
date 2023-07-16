1613e4f68de4139ab558ed56976c4ba4c9670649
Abastro <abab9579@gmail.com>
2023-07-16 10:33:04 +0900

    Install script changes

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+# shellcheck disable=SC2064
 cd "$( dirname "$0" )" || exit
+
+# Make it as a temporary file
+tmpdir=$( mktemp -d "${TMPDIR:-/tmp/}$(basename "$0").XXXXXXXXXXXX" )
+trap "rm -rf $tmpdir" EXIT # Want to expand it here
+
 # Statically built for consistency
-cabal install "exe:xmonad-manage" --overwrite-policy=always --disable-executable-dynamic --install-method=copy
+cabal install "exe:xmonad-manage" --disable-executable-dynamic --install-method=copy --installdir="$tmpdir"
 echo ""
-xmonad-manage
+"$tmpdir"/xmonad-manage update
