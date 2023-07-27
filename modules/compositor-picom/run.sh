b53e8d28f9687c9d8ccca1428083fc4a246f5ac8
Abastro <abab9579@gmail.com>
2023-07-27 09:48:41 +0900

    Spawning instead of -b option.

--- modules/compositor-picom/run.sh
+++ modules/compositor-picom/run.sh
@@ -2,8 +2,9 @@
 # If the temporary config exists, load it.
 if [ -f "$1/picom.conf" ]; then
   echo "Picom configuration found, loading..."
-  picom -b --config "$1/picom.conf"
+  picom --config "$1/picom.conf" &
 else
   # TODO -C and -G are removed in recent versions, need to handle somehow.
-  picom -bcCGf --detect-rounded-corners -D 4 -I 0.02 -O 0.02 -e 0.8
+  picom -cCGf --detect-rounded-corners -D 4 -I 0.02 -O 0.02 -e 0.8 &
 fi
+echo "launched picom."
